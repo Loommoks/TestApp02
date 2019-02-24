a53e65bdc691a1294aa91bd34a8212baa912c055
Loommoks <ga.pafnutov@gmail.com>
2019-02-24 14:24:38 +0300

    RecyclerView v7 library dependency added.
     LaunchActivity temporary fixed.
    ItemsGalleryActivity - host for feature RecyclerView Fragment created

---
+++ app/src/main/java/su/zencode/testapp02/ItemsGalleryActivity.java
@@ -0,0 +1,13 @@
+package su.zencode.testapp02;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+
+public class ItemsGalleryActivity extends SingleFragmentActivity {
+
+
+    @Override
+    protected Fragment createFragment() {
+        return ItemsGalleryFragment.newInstance();
+    }
+}
--- app/src/main/java/su/zencode/testapp02/LaunchActivity.java
+++ app/src/main/java/su/zencode/testapp02/LaunchActivity.java
@@ -1,13 +1,26 @@
 package su.zencode.testapp02;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LaunchActivity extends AppCompatActivity {
+    private Button mLaunchButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch);
+
+        mLaunchButton = findViewById(R.id.launch_button);
+        mLaunchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LaunchActivity.this, ItemsGalleryActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
